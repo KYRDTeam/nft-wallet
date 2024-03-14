092df8a52e2705f18e8489c0ba3fd8a8af869897
haupc <haupc.uet@gmail.com>
2024-03-14 15:53:32 +0700

    chore: move nft to foundry

--- contracts/script/SmartNftWallet.s.sol
+++ contracts/script/SmartNftWallet.s.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
+import "../src/SmartNftWallet.sol";
 
-contract CounterScript is Script {
+contract SmartNFTWalletScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+        new SmartNFTWallet();
+        vm.stopBroadcast();
     }
 }

595e4d194ac8bebaed240eb7d887794365069d1c
alonecandies <dragonfrezze@gmail.com>
2024-03-15 14:56:45 +0700

    update UI

--- extension/src/components/CreateWallet/index.tsx
+++ extension/src/components/CreateWallet/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Center } from "@chakra-ui/react";
+import { Box, Button, Center, Image } from "@chakra-ui/react";
 import { ReactComponent as Logo } from "src/assets/images/logos/nft-wallet.svg";
 import { ReactComponent as LogoPure } from "src/assets/images/logos/krystal-pure.svg";
 import { useAppSelector } from "src/hooks/useStore";
@@ -6,6 +6,7 @@ import { keysSelector } from "src/store/keys";
 import CreatePassword from "./CreatePassword";
 import Login from "./Login";
 import RestoreAccount from "./RestoreAccount";
+import CreateIllu from "src/assets/images/illus/create-illu.svg";
 
 const CreateWallet = () => {
   const { vault, isLoading } = useAppSelector(keysSelector);
@@ -14,15 +15,21 @@ const CreateWallet = () => {
     <>
       {isLoading ? null : (
         <Center h="calc(100vh - 60px)" flexDirection="column" px="8">
-          <Logo width="250" height="55" />
+          <Logo width="225" height="55" />
+          <Image src={CreateIllu} />
           {vault ? (
             <Login />
           ) : (
             <>
-              <Box w="100%" fontWeight="semibold" mt="100px" mb="5">
+              <Box w="100%" fontWeight="semibold" mb="5">
                 <CreatePassword
                   render={(open) => (
-                    <Button colorScheme="primary" w="100%" size="lg" onClick={open}>
+                    <Button
+                      colorScheme="primary"
+                      w="100%"
+                      size="lg"
+                      onClick={open}
+                    >
                       Create New Wallet
                     </Button>
                   )}
@@ -34,7 +41,13 @@ const CreateWallet = () => {
                 <RestoreAccount
                   case="RESTORE"
                   render={(onOpen) => (
-                    <Box display="inline" ml="1" color="primary.300" onClick={onOpen} cursor="pointer">
+                    <Box
+                      display="inline"
+                      ml="1"
+                      color="primary.300"
+                      onClick={onOpen}
+                      cursor="pointer"
+                    >
                       import using Secret Recovery Phrase
                     </Box>
                   )}
